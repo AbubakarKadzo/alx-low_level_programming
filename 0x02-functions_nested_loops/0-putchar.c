fa5c8eb5645ab87943544a15410ae75b1ef47bd0
AbubakarKadzo <husnabakar01@gmail.com>
2022-09-13 12:41:41 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Prints "_putchar" followed by a new line
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0 .
  *
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
