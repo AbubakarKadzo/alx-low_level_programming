081c0cac16fe6741e540af2d1bddbf5878e3bdc5
AbubakarKadzo <husnabakar01@gmail.com>
2022-09-15 10:22:25 -0700

    task2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,12 @@
 
 /**
  * mul -  checks for checks for a digit (0 through 9).
- * @a: a -  Variable
+ * @a: a - Variable
  * @b: b - variable
  * Return: Always 0.
  *
  */
+
 int mul(int a, int b)
 {
 	int mul;
