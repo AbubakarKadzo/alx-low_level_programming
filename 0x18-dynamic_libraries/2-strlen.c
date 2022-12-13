c69dd3ac32b5fe6c7f5f4d151e06ed36a78af55f
AKadzo <110997053+AbubakarKadzo@users.noreply.github.com>
2022-12-13 23:49:05 +0300

    strlen2

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: the string
- * Return: always length
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	int i = 1, sum = 0;
+	char pl = s[0];
 
-	while (*s != '\0')
+	while (pl != '\0')
 	{
-		s++;
-		len++;
+		sum++;
+		pl = s[i++];
 	}
-	return (len);
+	return (sum);
 }
