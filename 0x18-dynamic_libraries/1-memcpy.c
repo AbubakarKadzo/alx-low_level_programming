8fb61325f4961ec3d7648a866aea65802fb4a777
AKadzo <110997053+AbubakarKadzo@users.noreply.github.com>
2022-12-14 00:04:09 +0300

    memcpy1

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _memcpy - copies from the src to the dest
- * @dest: the dest
- * @src: the src
- * @n: the amount to be copied
- * Return: the altered dest
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *p = dest;
+	unsigned int i;
 
-	while (n--)
-	{
-		*p++ = *src++;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
