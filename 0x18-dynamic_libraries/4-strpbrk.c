0d3193b29adea345a390d66b374675ee1a9e67fe
AKadzo <110997053+AbubakarKadzo@users.noreply.github.com>
2022-12-14 00:09:16 +0300

    strpbrk4

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,39 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: the string
- * @accept: the bytes
- * Return: Where the bytes start to be found, or Null.
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, k, pos, Z = 0;
+	int i, n;
 
 	for (i = 0; s[i] != '\0'; i++)
-		;
-
-	pos = i;
-
-	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (k = 0; s[k] != '\0'; k++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (accept[i] == s[k])
-			{
-				if (k <= pos)
-				{
-					pos = k;
-					Z = 1;
-				}
-			}
+			if (s[i] == accept[n])
+				return (s + i);
 		}
 	}
-	if (Z == 1)
-	{
-		return (&s[pos]);
-	}
-	else
-	{
-		return (0);
-	}
+	return (NULL);
 }
